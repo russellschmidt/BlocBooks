af7a99c51062b635239524808941902ff18a50a8
Russell Schmidt <russell.schmidt@me.com>
2016-11-13 21:27:37 -0800

    completed checkpoint 2 work but not working

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ gem 'bloc_works', path: '../bloc_works'
 
 gem 'rake', '~> 10.0'
 gem 'rack', '~> 1.6'
+gem 'erubis', '~> 2.7'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require 'bloc_works'
 # $ bundle exec rackup -p 3000
 
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 
 module BlocBooks
 	class Application < BlocWorks::Application
