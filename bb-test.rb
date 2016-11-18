c56f317089e1189818d47e81b11a1d514536ce62
Russell Schmidt <russell.schmidt@me.com>
2016-11-17 22:21:31 -0800

    added a 3rd assertion to c2q2

--- bb-test.rb
+++ bb-test.rb
@@ -14,7 +14,9 @@ class BlocBooksTest < Test::Unit::TestCase
 		get '/books/welcome'
 		assert last_response.ok?
 		books_welcome_body = "<div>\n\t<p>Welcome to BlocBooks!</p>\n\t<p>Please start by reading Eloquent Ruby</p>\n</div>"
+		
 		assert_equal books_welcome_body, last_response.body
+		assert last_response.body.include?('Eloquent Ruby')
 	end
 
 end
