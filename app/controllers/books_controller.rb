9da5fb504ddde9228dd400f5c5b0ff6a168f2864
Russell Schmidt <russell.schmidt@me.com>
2016-11-28 16:38:14 -0800

    question 1 checkpoint 3 done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,16 @@ class BooksController < BlocWorks::Controller
 	end
 
 	def show
-		@my_book = "Eloquent Ruby"
-		# how do we pass in the /:id of the book
-		render :show, book: @my_book
+		# access env variable and get URI
+		# parse env variable to find '?id=1'
+		# parse the '1' out 
+		# 
+		uri = @env["REQUEST_URI"].to_s
+		id_query = uri.match(/\?([\w-])+(=[\w-]*)/).to_s
+		id = id_query.split('=')[1].to_i
+		# now we want to look up the record with id=1
+		# then we pass in that database object to render
+		render :show, book: Book.find(id)
 	end
 
 	def new
--- rakefile.rb
+++ rakefile.rb
@@ -1,8 +1,9 @@
-# require '/db/seed'
-
 namespace :db do
 	desc 'Seed the database using db/seed.rb'
 	task :seed do
-		puts 'hello world'
+		# check if table exists
+		ruby './db/create_books.rb'
+		# clear table data
+		ruby './db/seed.rb'
 	end
 end
